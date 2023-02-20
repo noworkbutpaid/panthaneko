c1fb04bdbda853783d6b4970bf134e56dc1282b4
jebetot <124641133+jebetot@users.noreply.github.com>
2023-02-20 14:10:03 -0600

    v2

--- assets/js/metamask.js
+++ assets/js/metamask.js
@@ -1,6 +1,24 @@
-var web3;
-
 async function Connect(){
-	await window.web3.currentProvider.enable();
-	web3=new Web3(window.web3.currentProvider);
+	document.getElementById("exampleModal").style.display = "none";
+	let el = document.getElementsByClassName("modal-backdrop");
+	while (el.length > 0) el[0].remove()
+	
+	if (window.ethereum) {
+		const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+		const account = accounts[0]
+		window.web3 = new Web3(window.ethereum);	
+
+    	const networkId = await web3.eth.net.getId()
+    	
+    	await window.ethereum.request({
+        	method: 'wallet_switchEthereumChain',
+        	params: [{ chainId: '0x38' }],
+    	});
+    	web3.eth.net.getId().then("chain id : " + console.log)	
+		
+		
+
+	} else {
+		alert("Please install MetaMask to use this dApp!");
+	}
 }
